e13ff3ea48255b6005775b3ebcfec543fcced615
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-08-17 18:12:07 +0530

    Pickup and Drop(Single weapon)

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -1,3 +1,4 @@
+using Mono.Reflection;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,24 +9,79 @@ public class PickupItem : MonoBehaviour
     // Start is called before the first frame update
     //public Transform gunPosition;
     public Transform gunPosition;
+    public GameObject gunonGround;
+    //public Transform player;
+    public Rigidbody rb;
+    public GameObject instruction;
+    public bool equipped = true;
+    public Transform fpsCam;
     
     void Start()
     {
+        instruction.SetActive(false);
+
+        if(!equipped)
+        {
+            Debug.Log("false");
+
+            rb.isKinematic = false;
+
+        }
+        else if(equipped)
+        {
+            rb.isKinematic = true;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            instruction.SetActive(true);
+            
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        instruction.SetActive(false);
         
     }
 
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (equipped && Input.GetKeyDown(KeyCode.G))
+        {
+            Drop();
+            
+        }
+       
     }
 
     public void Pickup()
     {
+        //gunonGround.SetActive(false);
+        rb.isKinematic = true;
         transform.SetParent(gunPosition);
         transform.localPosition = Vector3.zero;
         transform.localRotation = Quaternion.Euler(Vector3.zero);
         transform.localScale = Vector3.one;
+        equipped = true;
+        instruction.SetActive(false);
+    }
+
+    public void Drop()
+    {
+
+        transform.SetParent(null);
+            rb.isKinematic = false;
+            rb.velocity = Vector3.one;
+            rb.AddForce(fpsCam.forward * 1f, ForceMode.Impulse);
+           rb.AddForce(fpsCam.up * 1f, ForceMode.Impulse);
+        equipped = false;
 
         
     }
